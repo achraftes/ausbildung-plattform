b29d66ee5f5b25b4d0286fff49a627f6c1c3f1a8
achraf <achraf04chke@gmail.com>
2026-04-14 23:08:53 +0100

    chnage name  for title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>CareerHub – @yield('title', 'Startseite')</title>
+    <title>KarriereNabe – @yield('title', 'Startseite')</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet">
 
